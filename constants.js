15a009cac047d3974ba04e16a75a103b89055d8d
LunashaGit <lunamuylkens@gmail.com>
2024-03-12 22:15:19 +0100

    OOP - Script Begin

---
+++ constants.js
@@ -0,0 +1,7 @@
+class Constants {
+  defaultUrl = "https://www.google.com/travel/flights?hl=fr";
+  cookiesGoogleSearchPage = "Avant d'accéder à Google";
+  refuseCookies = "Tout refuser";
+}
+
+module.exports = { Constants };
